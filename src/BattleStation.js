2c7b6f3668ea65adb5e753a386963044aaf0c5d9
Nathan Goller-Deitsch <nathan@quartzic.co>
2023-09-09 23:05:16 -0400

    add basic ipc on React and bridge

--- src/BattleStation.js
+++ src/BattleStation.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
-import PreGameScreen from './components/PreGameScreen'
+import React from "react";
+import { useState } from "react";
+import PreGameScreen from "./components/PreGameScreen";
 
 export default function BattleStation() {
   const [players, setPlayers] = useState({
     player1: {
-      name: 'your mom',
-      accentColor: '#00ffff',
+      name: "your mom",
+      accentColor: "#00ffff",
     },
     player2: {
-      name: 'your dad',
-      accentColor: '#0000ff',
+      name: "your dad",
+      accentColor: "#0000ff",
     },
-  })
-  return <PreGameScreen players={players} setPlayers={setPlayers} />
+  });
+  let [drives, setDrives] = useState([]);
+  useEffect(() => {
+    window.electronAPI.onAddDrive((drive) => {
+      setDrives([...drives, drive]);
+    });
+    window.electronAPI.onRemoveDrive((drive) => {
+      setDrives(drives.filter((d) => d !== drive));
+    });
+  }, []);
+  return <PreGameScreen players={players} setPlayers={setPlayers} />;
 }
--- src/preload.js
+++ src/preload.js
@@ -1,18 +1,27 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require("electron");
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const dependency of ["chrome", "node", "electron"]) {
+    replaceText(`${dependency}-version`, process.versions[dependency]);
   }
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
+  ipcRenderer.on("devices", (_event, text) => replaceText("devices", text));
+});
 
-  ipcRenderer.on('devices', (_event, text) => replaceText('devices', text))
-})
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  onAddDrive: (callback) => ipcRenderer.on("drives:ADD_DRIVE", callback),
+  onRemoveDrive: (callback) => ipcRenderer.on("drives:REMOVE_DRIVE", callback),
+  updateFile: (file, contents) =>
+    ipcRenderer.invoke("files:UPDATE", { file, contents }),
+});
