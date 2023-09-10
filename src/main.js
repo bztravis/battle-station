0deb7c5738b002a2d675d5aa5807f436d12cb20f
Brian Travis <bztravis@umich.edu>
2023-09-09 22:02:33 -0400

    Get JSON from valid drives

--- src/main.js
+++ src/main.js
@@ -50,37 +50,48 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.\\
 
-let delayBeforeNext
-
 const checkFiles = async (prev) => {
   delayBeforeNext = 100
   // console.log('running')
   const drives = await drivelist.list()
 
   if (JSON.stringify(prev) != JSON.stringify(drives)) {
-    // console.log('PREVIOUS', JSON.stringify(prev))
-    // console.log('DRIVES', JSON.stringify(drives))
-    delayBeforeNext = 2000
     setTimeout(() => {
-      console.log('USB DRIVES', getUSBDrives(drives))
+      // console.log('USB DRIVES', getUSBDrives(drives))
+      const driveCandidates = getUSBDrives(drives)
+      let driveConfigs = []
+      for (let i = 0; i < driveCandidates.length; i++) {
+        // console.log('driveCandidates[i]', driveCandidates[i])
+        try {
+          if (fs.readdirSync(`${driveCandidates[i]}`).includes('.driveduels'))
+            driveConfigs = [
+              ...driveConfigs,
+              JSON.parse(
+                fs.readFileSync(`${driveCandidates[i]}/.driveduels`, 'utf8')
+              ),
+            ]
+          console.log('driveConfigs', driveConfigs)
+        } catch {
+          continue
+        }
+      }
     }, 1000)
   }
-
   setTimeout(() => {
     checkFiles(drives)
-  }, delayBeforeNext)
+  }, 100)
 }
 
 const startCheckFiles = async () => {
   checkFiles(await drivelist.list())
 }
-
 startCheckFiles()
 
 function getUSBDrives(drives) {
   let results = []
   for (let i = 0; i < drives.length; i++) {
-    if (drives[i].isUSB) results = [...results, JSON.stringify(drives[i])]
+    if (drives[i].isUSB && drives[i].mountpoints[0])
+      results = [...results, drives[i].mountpoints[0].path]
   }
 
   return results
