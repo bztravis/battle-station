c095f9a3e0e8aef91e56e57c78bb9db504cc2965
Brian Travis <bztravis@umich.edu>
2023-09-10 02:43:17 -0400

    Add v1 of gameplay state machine

--- src/components/GameView.js
+++ src/components/GameView.js
@@ -3,7 +3,55 @@ import { useState, useEffect } from 'react'
 import styles from '../styles/gameView.css'
 
 export default function GameView({ drives, setInGame }) {
-  const [playerTurn, setPlayerTurn] = useState(1)
+  const [playerTurn, setPlayerTurn] = useState(0)
+  const [activeBytelings, setActiveBytelings] = useState([null, null])
+  const [bytelings, setBytelings] = useState([
+    drives[0]?.bytelings,
+    drives[1]?.bytelings,
+  ])
+
+  const turnFlow = {
+    start: {
+      next: (bytelings) => 'actionP1',
+    },
+    chooseP1: {
+      next: (bytelings) => {
+        if (
+          bytelings[0].filter((elem) => elem.stats.currentHealth > 0).length ===
+          0
+        )
+          return 'winP2'
+        else return activeBytelings[1] ? 'chooseP2' : 'actionP1'
+      },
+    },
+    chooseP2: {
+      next: (bytelings) => {
+        if (
+          bytelings[1].filter((elem) => elem.stats.currentHealth > 0).length ===
+          0
+        )
+          return 'winP1'
+        else return activeBytelings[0] ? 'chooseP1' : 'actionP2'
+      },
+    },
+    actionP1: {
+      next: (bytelings) => {
+        return activeBytelings[1].stats.currentHealth <= 0
+          ? 'chooseP2'
+          : 'actionP2'
+      },
+    },
+    actionP2: {
+      next: (bytelings) => {
+        return activeBytelings[0].stats.currentHealth <= 0
+          ? 'chooseP1'
+          : 'actionP1'
+      },
+    },
+  }
+
+  // useEffect(() => {}, [playerTurn])
+
   console.log('drives', drives)
   return (
     <div className="container">
@@ -34,9 +82,11 @@ export default function GameView({ drives, setInGame }) {
       <div className="bytelingContainer">
         <div className="byteling">
           <div className="platform"></div>
+          <div className=""></div>
         </div>
         <div className="byteling">
           <div className="platform"></div>
+          <div className=""></div>
         </div>
       </div>
     </div>
